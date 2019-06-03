8573fba2e361b36ea371857653b20ae25e66f837
jimchen <jimchen0605@qq.com>
2019-06-04 00:21:54 +0800

    relation inference

--- maskrcnn_benchmark/modeling/roi_heads/relation_head/inference.py
+++ maskrcnn_benchmark/modeling/roi_heads/relation_head/inference.py
@@ -15,9 +15,14 @@ class RelationPostProcessor(nn.Module):
     def __init__(self):
         super(RelationPostProcessor, self).__init__()
 
-    def forward(self):
-        pass
-
+    def forward(self, x, boxes):
+        pos = 0
+        for box in boxes:
+            rel_val = x[pos:pos+len(box)]
+            rel_val = torch.squeeze(rel_val, dim=1)
+            pos += len(box)
+            box.add_field("relation_val", rel_val)
+        return boxes
 
 def make_roi_relation_post_processor(cfg):
     relation_post_processor = RelationPostProcessor()
